2adca52564f29673732d1cabab4845aede6ca25e
JyTO <jo.ticona@duocuc.cl>
2026-06-03 14:48:37 -0500

    chore: hardcode debug True

--- Producto/backend/config/settings.py
+++ Producto/backend/config/settings.py
@@ -33,8 +33,8 @@ environ.Env.read_env(BASE_DIR / '.env')
 # ---------------------------------------------------------------------------
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env('DEBUG')
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1', '192.168.1.8'])
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 
 # ---------------------------------------------------------------------------
 # Aplicaciones instaladas
